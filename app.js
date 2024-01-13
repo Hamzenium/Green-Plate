c158af241d850e65f6299c0cbbf9a2fb31fbee39
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2024-01-13 12:27:28 -0500

    Create Users

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const admin = require("firebase-admin");
+const credentials = require('./key.json');
+admin.initializeApp({
+	credential: admin.credential.cert(credentials)
+});
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const db = admin.firestore();
+app.post("/create/user", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const userData = {
+      name: req.body.name,
+      email: req.body.email,
+      preference: [],
+      recipee: []
+    };
+
+    await db.collection('users').doc(email).set(userData);
+
+    const response = { message: "User created successfully." };
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
